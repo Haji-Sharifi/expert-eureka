106f33834be6b6f5975f526bf68d270197030900
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-03-08 22:07:18 +0430

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Features from "./components/Features";
 
 function App() {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden bg-zinc-600">
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 }
